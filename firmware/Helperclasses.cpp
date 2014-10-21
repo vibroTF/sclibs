cf91389f3ab55fa3f1349c2e8db4dc9986ca5e6c
vibrotf <vibrotf@yahoo.de>
2014-10-21 19:07:47 +0200

    rest was sc will

---
+++ firmware/Helperclasses.cpp
@@ -0,0 +1,11 @@
+#include "Event.h"
+
+Event::Event(int acId, int inten, int tinte, int dura, int pa)
+{
+	this->acId=acId;
+	this->inten=inten;
+	this->tinte=tinte;
+	this->dura=dura;
+	this->pa=pa;
+
+}
