d31d8d11207815394fc8a36ade80eb722d0f9a76
vibrotf <vibrotf@yahoo.de>
2014-10-21 19:33:25 +0200

    added calculations of intensities

--- firmware/Event.cpp
+++ firmware/Event.cpp
@@ -2,9 +2,10 @@
 
 Event::Event(int acId, int inten, int tinte, int dura, int pa)
 {
+	double dinten, dtinte;
 	this->acId=acId;
-	this->inten=inten;
-	this->tinte=tinte;
+	this->inten=dinten/100*256;
+	this->tinte=dtinte/100*256;
 	this->dura=dura;
 	this->pa=pa;
 
